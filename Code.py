4bed79cf289b3a7fa92477e5dd56c6bfaceaa876
Saksham Gupta <saksham.gupta2018@vitstudent.ac.in>
2019-11-28 14:17:39 +0530

    Create Code.py
    I have also used an algorithm which I found on medium to generate big prime numbers for the key generation for the RSA

---
+++ Code.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 15:34:08 2019
+
+@author: sakshamgupta
+"""
+import random
+import time
+start_time = time.time()
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+def multiplicative_inverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+    
+    while e > 0:
+        temp1 = temp_phi//e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+        
+        x = x2- temp1* x1
+        y = d - temp1 * y1
+        
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+    
+    if temp_phi == 1:
+        return d + phi
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+    #n = pq
+    n = p * q
+    phi = (p-1) * (q-1)
+    #Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+    #Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    d = multiplicative_inverse(e, phi)
+    
+    #Return public and private keypair
+    #Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+def encrypt(pk, plaintext):
+    #Unpack the key into it's components
+    key, n = pk
+    #Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    cipher = [pow(ord(char),key,n) for char in plaintext]
+    #Return the array of bytes
+    return cipher
+
+def decrypt(pk, ciphertext):
+    #Unpack the key into its components
+    key, n = pk
+    #Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr(pow(char, key, n)) for char in ciphertext]
+    #Return the array of bytes as a string
+    return ''.join(plain)
+    
+from random import randrange, getrandbits
+def is_prime(n, k=128):
+    """ Test if a number is prime
+        Args:
+            n -- int -- the number to test
+            k -- int -- the number of tests to do
+        return True if n is prime
+    """
+    # Test if n is not even.
+    # But care, 2 is prime !
+    if n == 2 or n == 3:
+        return True
+    if n <= 1 or n % 2 == 0:
+        return False
+    # find r and s
+    s = 0
+    r = n - 1
+    while r & 1 == 0:
+        s += 1
+        r //= 2
+    # do k tests
+    for _ in range(k):
+        a = randrange(2, n - 1)
+        x = pow(a, r, n)
+        if x != 1 and x != n - 1:
+            j = 1
+            while j < s and x != n - 1:
+                x = pow(x, 2, n)
+                if x == 1:
+                    return False
+                j += 1
+            if x != n - 1:
+                return False
+    return True
+def generate_prime_candidate(length):
+    """ Generate an odd integer randomly
+        Args:
+            length -- int -- the length of the number to generate, in bits
+        return a integer
+    """
+    # generate random bits
+    p = getrandbits(length)
+    # apply a mask to set MSB and LSB to 1
+    p |= (1 << length - 1) | 1
+    return p
+def generate_prime_number(length=1024):
+    """ Generate a prime
+        Args:
+            length -- int -- length of the prime to generate, in          bits
+        return a prime
+    """
+    p = 4
+    # keep generating while the primality test fail
+    while not is_prime(p, 128):
+        p = generate_prime_candidate(length)
+    return p
+if __name__ == '__main__':
+    '''
+    Detect if the script is being run directly by the user
+    '''
+    print("RSA Encrypter/ Decrypter")
+    p = generate_prime_number()
+    q = generate_prime_number()
+    print("P=")
+    print(p)
+    print("Q=")
+    print(q)
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print ("Your public key is ", public ," and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print(type(encrypted_msg[0]))
+    print ("Your encrypted message is: ")
+    print (''.join(map(lambda x: str(x), encrypted_msg)))
+    print ("Decrypting message with public key ", public ," . . .")
+    print ("Your message is:")
+    
+    print (decrypt(public, encrypted_msg))
+    print("--- %s seconds ---" % (time.time() - start_time))
